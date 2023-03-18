682dbabbfe9325fbdd7d7e5c08319ac90ee3e686
jeffreykng@hotmail.com <jeffreyng@Jeffreys-MBP.fios-router.home>
2023-03-18 12:29:18 -0400

    for/while loops assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const lista =[]
+function writeCards(names){
+    
+for (let i = 0 ; i < names.length; i++){  
+    lista.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return lista
+}
+
+
+
+
+
+
+function countDown(x){
+    let iter = 10
+    while (iter >= 0) {
+        console.log(iter--)
+}}
+
+
